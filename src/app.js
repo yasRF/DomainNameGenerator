7b7cb92285b17421b8755e32bf5864886d859b2d
yasRF <yas070788@hotmail.es>
2022-10-04 15:13:20 +0000

    mi primer commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+let domainGenerator = (pronoun, adj, noun) => {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let r = 0; r < noun.length; r++) {
+        console.log(pronoun[i] + adj[j] + noun[r] + ".es");
+      }
+    }
+  }
 };
+domainGenerator(pronoun, adj, noun);
